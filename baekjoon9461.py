20529e52260071cdae33460742c8ef5b6747ead0
rincher <skymegod@gmail.com>
2021-03-13 23:15:01 +0900

    Update baekjoon9461.py

--- baekjoon9461.py
+++ baekjoon9461.py
@@ -1,10 +1,21 @@
-1 = 1
-2 = 1
-3 = 1
-4 = 2
-5 = 2
-6 = 3
-7 = 4
-8 = 5
-9 = 7
-10 = 9
+import sys
+
+T = int(sys.stdin.readline())
+P = {
+    1: 1, 2: 1, 3: 1, 4: 2, 5: 2, 6: 3, 7: 4, 8: 5, 9: 7, 10: 9
+}
+
+
+def find(N):
+    if N in P:
+        return P[N]
+
+    if N not in P:
+        P[N] = find(N-3)+find(N-2)
+        return P[N]
+
+
+while T:
+    N = int(sys.stdin.readline())
+    print(find(N))
+    T -= 1
